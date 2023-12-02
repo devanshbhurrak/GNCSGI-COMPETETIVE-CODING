0476e53d4d6499a768315180a5bdbf3162dda31b
Devansh Bhurrak <144364530+devanshbhurrak@users.noreply.github.com>
2023-12-02 21:44:06 +0530

    Create Defeat The Monster 1.cpp
    The problem involves determining whether it is possible to defeat a monster with a given initial health (H), damage dealt per hit (X), and health gained by the monster before each hit (Y). The goal is to output 1 if it is possible to reduce the monster's health to zero or less, and 0 otherwise. The input consists of the number of test cases (T) and, for each test case, three integers representing H, X, and Y.

---
+++ Defeat The Monster 1.cpp
@@ -0,0 +1,16 @@
+// Problem : https://www.codechef.com/practice/course/interview-dsa/DSAPREP_01/problems/MONSTER1?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    long long h,x,y;
+	    cin>>h>>x>>y;
+	    
+	    cout<<(x>y ? 1 : 0)<<endl;
+	}
+	return 0;
+}
