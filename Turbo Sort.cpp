d464ddb1a11d4c9e41c0f1de303dd3e3aeac80dd
Devansh Bhurrak <144364530+devanshbhurrak@users.noreply.github.com>
2023-12-04 22:56:45 +0530

    Create Turbo Sort.cpp
    The problem requires sorting a list of integers in non-decreasing order. The input consists of an integer N, representing the number of integers in the list, followed by N lines containing individual integers. The task is to output N lines with the sorted integers in non-decreasing order.

---
+++ Turbo Sort.cpp
@@ -0,0 +1,50 @@
+//Problem : https://www.codechef.com/problems/TSORT
+
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; --i)
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i > 0; --i) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+int main() {
+  int n;
+  cin >> n;
+
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  heapSort(arr, n);
+
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << endl;
+  }
+
+  return 0;
+}
