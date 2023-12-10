b07f6bfc8360047fdc598da220df06f02698f7f9
Devansh Bhurrak <144364530+devanshbhurrak@users.noreply.github.com>
2023-12-11 00:00:09 +0530

    Create Palindrome Number.cpp
    Given an integer x, return true if x is a 
    palindrome
    , and false otherwise.

---
+++ Palindrome Number.cpp
@@ -0,0 +1,18 @@
+// Problem : https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0 || (x!=0 && x%10==0))
+            return false;
+
+        long long rev = 0;
+
+        while(x > rev) {
+            rev = rev*10 + x%10;
+            x /= 10;
+        }
+
+        return (rev == x || rev/10 == x);
+    }
+};
